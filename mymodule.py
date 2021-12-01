b2b29af9d64587cc8c3727e58e168f6c9919994e
PattyCutie <iamsureiamcute@gmail.com>
2021-12-01 11:50:03 +0000

    Testing Unit project

---
+++ mymodule.py
@@ -0,0 +1,17 @@
+def square(number):
+    """
+    This function return the square of a given number
+    """
+    return number **2
+
+def double(number):
+    """
+    This function return twice thevalue of a given number
+    """
+    return number *2
+
+def add(a,b):
+    """
+    This function return the sum of the given number
+    """
+    return a + b
